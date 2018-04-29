547569b89cfea76519af086d08e5e8859fb878fd
stefano <stefano.bromuri@ou.nl>
2018-04-29 21:12:50 +0200

    committing changes to bat for the location of the mongodb

--- PythonScripts/AppStart.bat
+++ PythonScripts/AppStart.bat
@@ -1,3 +1,5 @@
 set VERA_EMOTION_PROCESSOR_ADDRESS=http://vera.northeurope.cloudapp.azure.com:50001/annotate
+set VERA_FEATURES_DB=mongodb://127.0.0.1:27017/VERAPreProcessor
 echo %VERA_EMOTION_PROCESSOR_ADDRESS%
+echo %VERA_FEATURES_DB%
 python app.py
